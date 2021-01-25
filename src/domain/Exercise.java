3785f8359bb7e37487d47be76b8c62bba6b2d190
Rifleborn <49074281+Rifleborn@users.noreply.github.com>
2021-01-25 19:46:58 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,10 +1,12 @@
 package domain;
+import java.security.*;
 
 /**
  * There 3 tasks. Every task have own method which call's by user in class TestResult
  * @author User
  */
 public class Exercise {
+        
     public static void Task1(int n) {
         System.out.println("Передане число: " + n);
         System.out.println(Integer.toBinaryString(n) + ", " + Integer.toOctalString(n) + ", " + Integer.toHexString(n) + "\n");
@@ -22,5 +24,28 @@ public class Exercise {
     }
     return true;
     }
+    
+    public static boolean Task3(String str1, String str2){
+         if (str1.length() != str2.length()) {
+        return false;
+    }
+
+    int[] letters = new int[256];
+
+    char[] s_array = str1.toCharArray();
+    for (char c : s_array) {
+        letters[c]++;
+    }
+
+    for (int i = 0; i < str2.length(); i++) {
+        int c = (int) str2.charAt(i);
+        if (--letters[c] < 0) {
+            return false;
+        }
+    }
+
+    return true;
+    }
+
 }
 
