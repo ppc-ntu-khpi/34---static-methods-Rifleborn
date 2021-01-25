7b5eea096fc6231d18c6e9ad313e57c988ad2e5e
Rifleborn <49074281+Rifleborn@users.noreply.github.com>
2021-01-25 18:52:00 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,26 @@
 package domain;
 
+/**
+ * There 3 tasks. Every task have own method which call's by user in class TestResult
+ * @author User
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static void Task1(int n) {
+        System.out.println("Передане число: " + n);
+        System.out.println(Integer.toBinaryString(n) + ", " + Integer.toOctalString(n) + ", " + Integer.toHexString(n) + "\n");
+    }
+    
+    public static boolean Task2(char[] word){
+    int i1 = 0;
+    int i2 = word.length - 1;
+    while (i2 > i1) {
+        if (word[i1] != word[i2]) {
+            return false;
+        }
+        ++i1;
+        --i2;
+    }
+    return true;
     }
 }
+
