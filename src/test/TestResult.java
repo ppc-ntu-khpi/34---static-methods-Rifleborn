763bc00db010751570d69d299f698072ffc4cf07
Rifleborn <49074281+Rifleborn@users.noreply.github.com>
2021-01-25 19:46:42 +0200

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,27 @@
 package test;
 
 import domain.Exercise;
+import java.util.Scanner;
 
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Task 1: n");
+        Exercise.Task1(123456);
+        
+        System.out.println("Task 2: ");
+        String s = sc.nextLine();
+        char ch[] = s.toCharArray();
+        System.out.println(Exercise.Task2(ch));
+        
+        System.out.println("Task 3: ");
+        String str1, str2;
+        System.out.println("Введіть дві строки");
+        str1 = sc.nextLine();
+        str2 = sc.nextLine();
+        System.out.println(Exercise.Task3(str1, str2));
+    
     }
 }
